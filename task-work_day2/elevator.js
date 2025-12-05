524f8682cc7c37e29927b6f094e20c9a33a2107f
catherine <catherinepgeorge02@gmail.com>
2025-12-05 11:15:38 +0530

    Modified file of elevator.js

--- task-work_day2/elevator.js
+++ task-work_day2/elevator.js
@@ -3,7 +3,7 @@ var floor = 3.5;
 console.log("Enter the floor number");
 console.log(`${floor}`);
 
-if (floor >= 5 || floor != Math.floor(floor)){
+if (floor > 5 || floor != Math.floor(floor) || floor < 1){
     console.log("This elevator doesn't go there!");
     console.log("Enter the floor number");
     
