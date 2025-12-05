b47cc161bbe0ac72e1328393f82df4f6859b528a
catherine <catherinepgeorge02@gmail.com>
2025-12-05 07:57:16 +0530

    Modified of file echo.js

--- task-work_day2/echo.js
+++ task-work_day2/echo.js
@@ -1,11 +1,11 @@
 var sent = "hello world"
 
-console.log("Enter a sentence");
+console.log("Enter a sentence:");
 console.log(`${sent}`);
 
 if(sent != "bye"){
     console.log("You said:",sent);
-    
+    console.log("Enter a sentence:");
 }
 else{
     console.log("Conversation ended.");
