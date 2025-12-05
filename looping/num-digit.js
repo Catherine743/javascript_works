8333bfffdd99f0d63ea410183377395851217a6c
catherine <catherinepgeorge02@gmail.com>
2025-12-05 11:25:33 +0530

    Modified file of num-digit.js

--- looping/num-digit.js
+++ looping/num-digit.js
@@ -1,11 +1,10 @@
-var num = 3456;
+var num = 345; // num = 345
 
-noDigit = 0;
+var count = 0; // count = 0
 
-while (num != 0){
-    digit = num % 10;
-    num = Math.floor(num / 10)
-    noDigit++;
-    
+while (num != 0){ // 3456 != 0 =>T, 34 != 0 =>T, 3 != 0 =>T, 0 != 0 =>F
+    num = Math.floor(num / 10) // num = 345 / 10 =>34, 34 / 10 =>3,num = 3 / 10 = 0
+    count++; // 3
 }
-console.log(noDigit);
+
+console.log(count); // 3
