96a800793fc3ad691737576bd24d418714c9d20f
catherine <catherinepgeorge02@gmail.com>
2025-12-06 17:08:26 +0530

    Added program of gcd of 2 numbers

--- looping - for/highest-div.js
+++ looping - for/highest-div.js
@@ -1,5 +1,21 @@
-var num1 = 12;
-var num2 = 48;
+var num1 = 24;
+var num2 = 18;
+var div = 0;
+var min = 0;
 
-var max = 0;
+if (num1 < num2) {
+    min = num1;
+}
+else {
+    min = num2;
+}
 
+for (i = 1; i <= min; i++) {
+    if (num1 % i == 0 && num2 % i == 0) {
+        if (div < i) {
+            div = i;
+        }
+    }
+}
+
+console.log(`GCD of ${num1} and ${num2} is ${div}`);
