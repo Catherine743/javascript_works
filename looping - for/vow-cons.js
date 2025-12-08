fcd69d68dbe7d9856c01f9b2163f860c82cad292
catherine <catherinepgeorge02@gmail.com>
2025-12-08 13:16:05 +0530

    Modified code vow-cons.js

--- looping - for/vow-cons.js
+++ looping - for/vow-cons.js
@@ -1,12 +1,13 @@
 var vCount = 0;
 var cCount = 0;
-var text = "Welcome";
+var text = "Hello Javascript";
+text.toLowerCase();
 
 for(let ch of text){
     if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
         vCount++;
     }
-    else{
+    else if(ch != ' '){
         cCount++;
     }
 }
