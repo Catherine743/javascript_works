4f02081e7cea5d835533c5a469b9614d89a09035
catherine <catherinepgeorge02@gmail.com>
2025-12-14 21:01:25 +0530

    Modified freq-count.js

--- task-work_day8 - string/freq-count.js
+++ task-work_day8 - string/freq-count.js
@@ -4,7 +4,6 @@ for (let i = 0; i < word.length; i++) {
     let count = 0;
     let ch = word[i];
 
-    // skip already counted characters
     let alreadyCounted = false;
     for (let k = 0; k < i; k++) {
         if (word[k] === ch) {
@@ -15,7 +14,6 @@ for (let i = 0; i < word.length; i++) {
 
     if (alreadyCounted) continue;
 
-    // count frequency
     for (let j = 0; j < word.length; j++) {
         if (word[j] === ch) {
             count++;
