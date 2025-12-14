67a453bbbea869cad7ae37b5ebe3ab75d5c4c17c
catherine <catherinepgeorge02@gmail.com>
2025-12-14 21:05:18 +0530

    Modified reverse.js

--- task-work_day8 - string/reverse.js
+++ task-work_day8 - string/reverse.js
@@ -4,14 +4,13 @@ let temp = "";
 
 for (let i = 0; i < words.length; i++) {
     if (words[i] !== ' ') {
-        temp = words[i] + temp;   // reverse current word
+        temp = words[i] + temp;   
     } else {
         result = result + temp + " ";
         temp = "";
     }
 }
 
-// add last word
 result = result + temp;
 
 console.log(result);
