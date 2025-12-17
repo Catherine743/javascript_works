768f6968a5fedf712466b9347ff02575b49a9e3f
catherine <catherinepgeorge02@gmail.com>
2025-12-17 20:37:15 +0530

    Modified count-words.js

--- task-work_day9/count-words.js
+++ task-work_day9/count-words.js
@@ -1,13 +1,42 @@
-let text = "Hello     javascript";
-let count = 0;
+let text = "Hello     JavaScript   world";
+text = text.trim();                 // remove extra spaces
 
-for (let i = 0; i < text.length; i++) {
-    if (text[i] != ' ' && (i == 0 || text[i - 1] == ' ')) {
-        count++;
-    }
-}
+let words = text.split(/\s+/);      // split by one or more spaces
+let count = words.length;
 
-console.log(`No of words in "${text}" is ${count}`);
+console.log("Number of words:", count);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let text = "Hello     javascript";
+// let count = 0;
+
+// for (let i = 0; i < text.length; i++) {
+//     if (text[i] != ' ' && (i == 0 || text[i - 1] == ' ')) {
+//         count++;
+//     }
+// }
+
+// console.log(`No of words in "${text}" is ${count}`);
+
 
 
 
