fe5ef7bb00b68c1deebd29297245458bdffc1c14
catherine <catherinepgeorge02@gmail.com>
2025-12-17 10:44:37 +0530

    Modified contains-word.js

--- task-work_day9/count-words.js
+++ task-work_day9/count-words.js
@@ -1,6 +1,15 @@
-let text = "Hello javascript";
-let count = text.split(" ").length;
+let text = "Hello     javascript";
+let count = 0;
+
+for (let i = 0; i < text.length; i++) {
+    if (text[i] != ' ' && (i == 0 || text[i - 1] == ' ')) {
+        count++;
+    }
+}
+
 console.log(`No of words in "${text}" is ${count}`);
+
+
 
 
 
