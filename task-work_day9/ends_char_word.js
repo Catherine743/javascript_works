65a1d5cec9703d4a98432ffd4d032ae6fef45880
catherine <catherinepgeorge02@gmail.com>
2025-12-17 12:58:43 +0530

    Modified ends_char_word.js

--- task-work_day9/ends_char_word.js
+++ task-work_day9/ends_char_word.js
@@ -26,4 +26,28 @@ else {
     } 
 }
 
+// let text = "hello javascript";
+// word = "javascript"
+
+// text = text.trim();
+
+// let lastChar = text.at(-1);
+// console.log("Last character ->", lastChar);
+
+// let lastWord = "";
+// for (let i = text.length - 1; i >= 0; i--) {
+//     if (text[i] == " ") {
+//         lastWord = text.substring(i + 1);
+//         break;
+//     }
+// }
+
+
+// if (lastWord == "") {
+//     lastWord = text;
+// }
+
+// console.log("Last word ->", lastWord);
+
+
 
