caee2609b0202f81193ca149ec3fb4c01579a385
catherine <catherinepgeorge02@gmail.com>
2025-12-17 20:16:48 +0530

    Modified cnt-no-words.js

--- task-work_day9/cnt-no-words.js
+++ task-work_day9/cnt-no-words.js
@@ -24,3 +24,26 @@ console.log(`No of words in sent is ${count}`);
 
 
 
+
+
+// sent = "sea seashore sea";
+// word = "sea";
+// count = 0;
+// used = "";
+
+// for (let ch of sent) {
+//     for (i = 0; i < word.length; i++) {
+//         if (ch == word[i]) {
+//             used = used + word[i];
+//         }
+//     }
+//     if (word == used) {
+//         count++;
+//     }
+// }
+// console.log(`No of words is ${count}`);
+
+
+
+
+
