d41cc272de35ab180c79b056ca8af451f4f44933
catherine <catherinepgeorge02@gmail.com>
2025-12-21 18:40:27 +0530

    Modified task-works of day_14

--- task-work_day14/median.js
+++ task-work_day14/median.js
@@ -3,5 +3,6 @@ let numbers = [12,5,20,8,15,30,7,25,10,18];
 numbers.sort((n1,n2) => n1 - n2);
 mid = Math.floor(numbers.length / 2);
 median = numbers.length % 2 !== 0? numbers[mid]: (numbers[mid - 1] + numbers[mid]) / 2;
-console.log(median);
+numGreatMedian = numbers.filter(n => n > median);
+console.log(numGreatMedian);
 
--- task-work_day14/sec-high-val.js
+++ task-work_day14/sec-high-val.js
@@ -1,5 +1,5 @@
 let numbers = [12,5,20,8,15,30,7,25,10,18];
 
 numbers.sort((n1,n2) => n2 - n1);
-console.log(numbers[1]);
+console.log(`Second highest value is ${numbers[1]}`);
 
