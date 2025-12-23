e5413f2eda05a3dd16c26438781f135ee9d4f354
catherine <catherinepgeorge02@gmail.com>
2025-12-23 11:02:06 +0530

    Added task-works of day_15

---
+++ task-work_day15/student.js
@@ -0,0 +1,41 @@
+student = {
+    name : "Rachel",
+    age : 20,
+    course : "Mearn Stack",
+    phoneNo : 9445672341
+}
+
+console.log(student.name);
+
+student.email = "rachel@gmail.com"
+console.log(student);
+
+// student.age += 5;
+// console.log(student);
+
+// if("address" in student){
+//     console.log("Address exists");
+    
+// }
+// else{
+//     console.log("Address not exists");
+    
+// }
+
+// count = Object.keys(student).length;
+// console.log(count);
+
+// console.log(Object.keys(student));
+
+// console.log(Object.values(student));
+
+// delete student.phoneNo;
+// console.log(student);
+
+
+
+
+
+
+
+
