4300db76d66cf19208e7394cc3594ca7d29efe88
catherine <catherinepgeorge02@gmail.com>
2025-12-29 11:01:29 +0530

    Modified task work of day_15

--- task-work_day15/student.js
+++ task-work_day15/student.js
@@ -1,3 +1,5 @@
+// Create an object named 'student' with properties: name, age, and course
+
 student = {
     name : "Rachel",
     age : 20,
@@ -5,14 +7,18 @@ student = {
     phoneNo : 9445672341
 }
 
+// Display the name property
 console.log(student.name);
 
+// Add a new property 'email' 
 student.email = "rachel@gmail.com"
 console.log(student);
 
+// Update the age value
 student.age += 5;
 console.log(student);
 
+// Check if the property 'address' exists
 if("address" in student){
     console.log("Address exists");
     
@@ -22,16 +28,21 @@ else{
     
 }
 
+// Count the total number of properties
 count = Object.keys(student).length;
 console.log(count);
 
+// Display all keys
 console.log(Object.keys(student));
 
+// Display all values
 console.log(Object.values(student));
 
+//Delete the phone number
 delete student.phoneNo;
 console.log(student);
 
+// Loop through an object and display key-value pairs
 for(let key in student){
     console.log(key,":",student[key]);
     
