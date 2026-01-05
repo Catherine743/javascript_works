78ab6ee5438903d5abf705863a34341e1ffd881e
catherine <catherinepgeorge02@gmail.com>
2026-01-05 20:34:53 +0530

    Added task works of day_6

--- task-work_day6/full-pym.js
+++ task-work_day6/full-pym.js
@@ -1,22 +1,42 @@
-let row = 5;
-let col = 9;
+rows = 5;
 
-for (let r = 1; r <= row; r++) {
-    pattern = ""; 
-    let i = 0; 
+for (i = 1; i <= rows; i++) {
+    pattern = "";
 
-    for (let c = 1; c <= col; c++) {
+    // spaces
+    for (s = 1; s <= rows - i; s++) {
+        pattern += "\t";
+    }
 
-        
-        if ((r + c == 6 || r + c == 8 || r + c == 10 || r + c == 12 || r + c == 14) && i < r) {
-            pattern = pattern + "*\t";   
-            i++;                   
-        }
-        else{
-            pattern = pattern + "\t";
-        }
-
+    // stars
+    for (j = 1; j <= (2 * i - 1); j++) {
+        pattern += "*\t";
     }
 
     console.log(pattern);
 }
+
+// or
+
+// row = 5;
+// col = 9;
+
+// for (r = 1; r <= row; r++) {
+//     pattern = ""; 
+//     i = 0; 
+
+//     for ( c = 1; c <= col; c++) {
+
+        
+//         if ((r + c == 6 || r + c == 8 || r + c == 10 || r + c == 12 || r + c == 14) && i < r) {
+//             pattern = pattern + "*\t";   
+//             i++;                   
+//         }
+//         else{
+//             pattern = pattern + "\t";
+//         }
+
+//     }
+
+//     console.log(pattern);
+// }
