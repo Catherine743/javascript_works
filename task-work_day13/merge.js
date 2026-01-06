ace0059b81a3b27c30b16d30130ba2ac81f76397
catherine <catherinepgeorge02@gmail.com>
2026-01-06 08:07:01 +0530

    Added task works of day_9

--- task-work_day13/merge.js
+++ task-work_day13/merge.js
@@ -1,6 +1,6 @@
-let arr1 = [32,44,59,71,22];
-let arr2 = [25,90,67,11];
-let newArr = [];
+arr1 = [32,44,59,71,22];
+arr2 = [25,90,67,11];
+newArr = [];
 
 for (let n of arr1) newArr.push(n);
 for (let n of arr2) newArr.push(n);
