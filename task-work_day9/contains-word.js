33ca5700173881578967beca3f859feb9111f8b9
catherine <catherinepgeorge02@gmail.com>
2026-01-06 07:36:30 +0530

    Added task works of day_9

--- task-work_day9/contains-word.js
+++ task-work_day9/contains-word.js
@@ -1,16 +1,16 @@
-let text = "JavaScript is Easy to Learn";
+text = "JavaScript is Easy to Learn";
 text = text.toLowerCase();
 
-let word = "javascript";
+word = "javascript";
 word = word.toLowerCase();
 
 words = "";
 
-let found = false;
-for (let i = 0; i < text.length; i++) {
-    let match = true;
+found = false;
+for (i = 0; i < text.length; i++) {
+    match = true;
 
-    for (let j = 0; j < word.length; j++) {
+    for (j = 0; j < word.length; j++) {
         if (text[i + j] != word[j]) {
             match = false;
             break;
