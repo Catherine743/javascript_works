a3985fee6584f131ab927c0b4a5aac9c8c3121c9
catherine <catherinepgeorge02@gmail.com>
2026-01-14 13:20:04 +0530

    Modified calculator work

---
+++ css_calculator/script.js
@@ -0,0 +1,12 @@
+// 1. Display number in text box
+function displayNum(num) {
+    result.value += num;
+}
+
+// 2. Clear text box
+function clearBox(){
+    result.value = ""
+}
+
+// 3. Evaluate expression
+
