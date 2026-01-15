41a573053f40b10c124e152efbb8ce4396d30ec7
catherine <catherinepgeorge02@gmail.com>
2026-01-15 21:25:16 +0530

    Modified task work of day_28

---
+++ task-work_day28/keyboard.js
@@ -0,0 +1,15 @@
+function alphaNum(alpNum) {
+    result.value += alpNum;
+}
+
+function backSpace() {
+    result.value = result.value.slice(0,-1)
+}
+
+function enterBtn() {
+    result.value += '\n'
+}
+
+function capsBtn() {
+    
+}
