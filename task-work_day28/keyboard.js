aa981f086d685676e98660bfc7ef7110b478c05b
catherine <catherinepgeorge02@gmail.com>
2026-01-16 21:09:19 +0530

    Modified task works of task work of day_28

--- task-work_day28/keyboard.js
+++ task-work_day28/keyboard.js
@@ -1,11 +1,32 @@
-function alphaNum(alpNum) {
-    result.value += alpNum;
+result = document.getElementById('result');
+
+function alphaNum(btn) {
+    result.focus()
+    result.value += btn.textContent;
 }
 
 function backSpace() {
+    result.focus();
     result.value = result.value.slice(0,-1)
 }
 
 function enterBtn() {
+    result.focus();
     result.value += '\n'
+}
+
+capsOn = true;
+function capsBtn() {
+    result.focus();
+    capsOn = !capsOn;
+    buttons = document.querySelectorAll('.alpha-btn')
+    buttons.forEach((button) => {
+        if (button.textContent == button.textContent.toUpperCase()) {
+            button.textContent = button.textContent.toLowerCase()
+        }
+        else {
+            button.textContent = button.textContent.toUpperCase()
+        }
+    })
+
 }
