db9c2bb673bf191b7164668544b6195edaacfd60
catherine <catherinepgeorge02@gmail.com>
2026-01-19 21:39:17 +0530

    Modified code of fill-missing-nums

--- array/fill-missing-nums.js
+++ array/fill-missing-nums.js
@@ -1,13 +1,23 @@
-var arr = [1,3,5,6,7,9];
-newArr = [];
-i = 1;
-j = 0;
+var arr = [1, 3, 5, 6, 7, 9];
+var newArr = [];
+var i = 1;
+var j = 0;
 
-for(let n of arr){
-     if(arr[i] != n){
+for (let n of arr) {
+    if (arr[i] != n) {
         newArr[j] = n;
-     }
-     else{
-        
-     }
+        j++;
+    } else {
+        temp = n;
+        while (temp < arr[i]) {
+            newArr[j] = temp;
+            j++;
+            temp++;
+        }
+        i++;
+    }
 }
+
+console.log(newArr);
+
+
