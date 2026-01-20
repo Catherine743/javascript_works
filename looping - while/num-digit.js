9cf857a221ffdb23962183b2ccda041fff9637c2
catherine <catherinepgeorge02@gmail.com>
2026-01-20 20:16:42 +0530

    Modified code of file in looping-while

--- looping - while/num-digit.js
+++ looping - while/num-digit.js
@@ -2,7 +2,7 @@ var num = 345; // num = 345
 
 var count = 0; // count = 0
 
-while (num != 0){ // 3456 != 0 =>T, 34 != 0 =>T, 3 != 0 =>T, 0 != 0 =>F
+while (num != 0){ // 345 != 0 =>T, 34 != 0 =>T, 3 != 0 =>T, 0 != 0 =>F
     num = Math.floor(num / 10) // num = 345 / 10 =>34, 34 / 10 =>3,num = 3 / 10 = 0
     count++; // 3
 }
