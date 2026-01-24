2a44b0e15b5b411ebd7c1e248af408781bb723e8
catherine <catherinepgeorge02@gmail.com>
2026-01-24 10:15:08 +0530

    Modified code of close-to-zero

--- close-to-zero.js
+++ close-to-zero.js
@@ -2,18 +2,69 @@
 // Ex: [1,-1,-2,3]
 // Note: If 2 numbers are at the same distance from zero, return the larger number.
 
-arr = [1, -1, -2, 3];
-nearest = arr[0];
+// arr = [1, -1, -2, 3];
+// nearest = arr[0];
 
-for (let n of arr) {
-    if (
-        Math.abs(n) < Math.abs(nearest) ||
-        (Math.abs(n) == Math.abs(nearest) && n > nearest)
-    ) {
-        nearest = n;
+// for (let n of arr) {
+//     if (
+//         Math.abs(n) < Math.abs(nearest) ||
+//         (Math.abs(n) == Math.abs(nearest) && n > nearest)
+//     ) {
+//         nearest = n;
+//     }
+// }
+
+// console.log(nearest);
+
+arr = [2, -1, -2, 3];
+nmin = -1;
+pmin = 1;
+
+for(let n of arr) {
+    if(n == nmin || n == pmin) {
+        if(n > 0) {
+            console.log(n);
+            break;
+        }
+    }
+    else {
+        nmin--;
+        pmin++;
     }
 }
 
-console.log(nearest);
+// Another way
 
+arr = [-1, -2, 2, 1, 3];
+nmin = -1;
+pmin = 1;
+found = false;
+
+while (!found) {
+
+    // check positive first (priority)
+    for (let n of arr) {
+        if (n == pmin) {
+            console.log(pmin);
+            found = true;
+            break;
+        }
+    }
+
+    // check negative only if positive not found
+    if (!found) {
+        for (let n of arr) {
+            if (n == nmin) {
+                console.log(nmin);
+                found = true;
+                break;
+            }
+        }
+    }
+
+    if (!found) {
+        nmin--;
+        pmin++;
+    }
+}
 
