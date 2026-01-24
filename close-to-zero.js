25a1fe7161e5861aeeb394b58b9d1c0eded2e0bc
catherine <catherinepgeorge02@gmail.com>
2026-01-24 09:56:39 +0530

    Modified code of close-to-zero

--- close-to-zero.js
+++ close-to-zero.js
@@ -2,36 +2,18 @@
 // Ex: [1,-1,-2,3]
 // Note: If 2 numbers are at the same distance from zero, return the larger number.
 
-// input = [1,-1,-2,3];
-// closest = null;
-
-// for(i = 0; i < input.length; i++) {
-//     num = input[i];
-//     if(closest == null) {
-//         closest = num;
-//     }
-//     else if(Math.abs(num) < Math.abs(closest)) {
-//         closest = num;
-//     }
-//     else if(Math.abs(num) == Math.abs(closest) && num > closest) {
-//         closest = num;
-//     }
-// }
-// console.log(closest);
- 
-// another way without using Math.abs
-
 arr = [1, -1, -2, 3];
-minDist = 9999;
+nearest = arr[0];
 
-for (n of arr) { // 1, -1
-    dist = n < 0 ? -n : n; // 1, 1
-
-    if (dist < minDist || (dist == minDist && n > ans)) { // 1 < 9999 => T // 1 == 1 && -1 > 1 => F
-        minDist = dist; // minDist = 1
-        ans = n; // ans = 1
+for (let n of arr) {
+    if (
+        Math.abs(n) < Math.abs(nearest) ||
+        (Math.abs(n) == Math.abs(nearest) && n > nearest)
+    ) {
+        nearest = n;
     }
 }
 
-console.log(ans);
+console.log(nearest);
+
 
