08fb03645e5f00b78d75338c1ca2964573a5424e
catherine <catherinepgeorge02@gmail.com>
2026-02-03 12:56:06 +0530

    Modified code of fact.js

--- task-work_day20 - recursion/fibanocci.js
+++ task-work_day20 - recursion/fibanocci.js
@@ -6,3 +6,10 @@ function fibanocci(n){
 }
 
 console.log(`Fibanocci of ${n} is ${fibanocci(n)}`);
+
+// fibanocci(7) = fibanocci(6) + fibanocci(5) // 8 + 5 = 13 ^
+// fibanocci(6) = fibanocci(5) + fibanocci(4) // 5 + 3 = 8  |
+// fibanocci(5) = fibanocci(4) + fibanocci(3) // 3 + 2 = 5  | 
+// fibanocci(4) = fibanocci(3) + fibanocci(2) // 2 + 1 = 3  |
+// fibanocci(3) = fibanocci(2) + fibanocci(1) // 1 + 1 = 2  |
+// fibanocci(2) = fibanocci(1) + fibanocci(0) // 1 + 0 = 1  |
